65a0d8b2e22902856977b1f67ef3851d6ebb5330
Kubantov Muhamedzhan <mukhamedzhan.kubantov@narxoz.kz>
2026-03-14 13:48:05 +0500

    Implement BasicAttack concrete component

--- src/com/narxoz/rpg/decorator/BasicAttack.java
+++ src/com/narxoz/rpg/decorator/BasicAttack.java
@@ -24,6 +24,6 @@ public class BasicAttack implements AttackAction {
     @Override
     public String getEffectSummary() {
         // TODO: Decide how a plain attack should describe its effects.
-        return "TODO";
+        return "Base physical damage";
     }
 }
